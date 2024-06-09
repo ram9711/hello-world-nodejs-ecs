b8a2897eaf4c45cbe7e0b766b2fb93836f0ba573
Chollangi Sri Ram <145741104+ram9711@users.noreply.github.com>
2024-06-09 21:53:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,66 +1,95 @@
+// main.tf
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_ecs_cluster" "example" {
-  name = "example"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-  ]
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_ecs_task_definition" "example" {
-  family                   = "example"
+resource "aws_security_group" "ecs" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "example"
-      image     = "YOUR_DOCKER_IMAGE_URL"
-      essential = true
-
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        },
-      ]
-    },
-  ])
+  container_definitions = jsonencode([{
+    name      = "hello_world"
+    image     = "YOUR_ECR_REPOSITORY_URL"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
 }
 
-resource "aws_ecs_service" "example" {
-  name            = "example"
-  cluster         = aws_ecs_cluster.example.id
-  task_definition = aws_ecs_task_definition.example.arn
+resource "aws_ecs_service" "hello_world" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
   desired_count   = 1
-
-  launch_type = "FARGATE"
-
+  launch_type     = "FARGATE"
   network_configuration {
-    subnets         = ["YOUR_SUBNET_ID"]
-    security_groups = ["YOUR_SECURITY_GROUP_ID"]
+    subnets         = [aws_subnet.public.id]
+    security_groups = [aws_security_group.ecs.id]
   }
+}
+
+resource "aws_lb" "main" {
+  name               = "hello-world-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs.id]
+  subnets            = [aws_subnet.public.id]
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "hello-world-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.main.dns_name
 }
